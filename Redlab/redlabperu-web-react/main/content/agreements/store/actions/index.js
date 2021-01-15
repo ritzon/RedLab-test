4e75d5c375cfdd432e687fa0f8753cdb944f98dd
ritzon <ritzon.sifuentesv@gmail.com>
2021-01-14 21:20:12 -0500

    Delete Redlab/redlabperu-web-react/main/content directory

--- Redlab/redlabperu-web-react/main/content/agreements/store/actions/index.js
+++
@@ -1,2 +0,0 @@
-export * from './agreements.actions';
-export * from './agreement.actions';
--- Redlab/redlabperu-web-react/main/content/global/examPDF.js
+++
@@ -1,9 +0,0 @@
-import { Document, Page } from '@react-pdf/renderer'
-
-export const examPDF = (
-	<Document>
-	    <Page>
-	      // My document data
-    	</Page>
-  	</Document>
-)
